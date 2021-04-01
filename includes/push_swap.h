30ff679c9f70f4ac0fc8f4dd2833e7b0aa039a17
Loïc <lbertran@student.42lyon.fr>
2021-04-01 14:47:10 +0200

    ptite organisation du .h ici laaa

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lbertran <lbertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 14:40:58 by lbertran          #+#    #+#             */
-/*   Updated: 2021/04/01 14:17:57 by lbertran         ###   ########lyon.fr   */
+/*   Updated: 2021/04/01 14:46:55 by lbertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,9 @@ typedef struct	s_range
 	int			max;
 }				t_range;
 
-
-void	exit_error(char *message);
-void	print_error(char *message);
+/*
+** Basic stack functions
+*/
 
 t_stack	*new_stack(int capacity);
 int		stack_size(t_stack *stack);
@@ -53,8 +53,12 @@ int		is_empty(t_stack *stack);
 void	stack_push(t_stack *stack, int val);
 int		stack_peek(t_stack *stack);
 int		stack_pop(t_stack *stack);
+
+/*
+** Stack utils
+*/
+
 t_stack	*stack_copy(t_stack *stack);
-void	print_stack(t_stack *stack);
 int		is_stack_sorted(t_stack *stack);
 void	reverse_stack(t_stack *tmp, t_stack *stack);
 int		stack_smallest(t_stack *stack);
@@ -62,6 +66,12 @@ int		stack_biggest(t_stack *stack);
 int		stack_smallest_index(t_stack *stack);
 int		stack_biggest_index(t_stack *stack);
 int		stack_get(t_stack *stack, int head);
+void	print_stack(t_stack *stack);
+
+/*
+** Stack sorting utils
+*/
+
 void	push_to_top_a(t_stack *stack, int index);
 void	push_to_top_b(t_stack *stack, int index);
 int		find_next_bigger(t_stack *stack, int current);
@@ -71,6 +81,12 @@ int		get_ideal_chunk_size(t_game *game, int biggest);
 int		find_in_range(t_stack *stack, t_game *game);
 int		find_median(t_stack *stack, int mid);
 int		find_median_value(t_stack *stack, int mid);
+int		stack_get(t_stack *stack, int head);
+int		actions_to_top(t_stack *stack, int index);
+
+/*
+** Push swap operations
+*/
 
 void	ps_push(t_stack *origin, t_stack *destination, char *msg);
 void	ps_rotate(t_stack *stack, char *msg);
@@ -79,13 +95,16 @@ void	ps_swap(t_stack *stack, char *msg);
 void	ps_ss(t_stack *stack_a, t_stack *stack_b);
 void	ps_rrotate(t_stack *stack, char *msg);
 void	ps_rrr(t_stack *stack_a, t_stack *stack_b);
-void	print_game(t_game *game);
-int		stack_get(t_stack *stack, int head);
+
+
+/*
+** Push swap functions
+*/
+
 void	sort_3(t_stack *stack);
 void	sort_5(t_game *game);
 void	sort(t_game *game);
-int		actions_to_top(t_stack *stack, int index);
-
+void	print_game(t_game *game);
 
 /*
 ** Parsing
@@ -102,5 +121,12 @@ void	parse_input(int ac, char **av, t_game *game);
 
 int		splitlen(char **split);
 int		ft_abs(int i);
+
+/*
+** Errors
+*/
+
+void	exit_error(char *message);
+void	print_error(char *message);
 
 #endif
