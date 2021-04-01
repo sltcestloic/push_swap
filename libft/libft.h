5ba703b1dace6b7a067438f8609ab84a10615ea0
Loïc <lbertran@student.42lyon.fr>
2021-04-01 15:30:40 +0200

    update libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lbertran <lbertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 19:59:28 by lbertran          #+#    #+#             */
-/*   Updated: 2021/03/28 15:08:22 by lbertran         ###   ########lyon.fr   */
+/*   Updated: 2021/04/01 15:13:55 by lbertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,10 @@ typedef struct s_list
 	struct s_list	*next;
 }				t_list;
 
-size_t			ft_strlen(const char *str);
+/*
+** Char
+*/
+
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
 int				ft_isalnum(int c);
@@ -43,34 +46,28 @@ int				ft_isprint(int c);
 int				ft_iswhitespace(char c);
 int				ft_toupper(int c);
 int				ft_tolower(int c);
-int				ft_strncmp(const char *s1, const char *s2, size_t n);
-int				ft_strcmp(const char *s1, const char *s2);
-size_t			ft_strlcpy(char *dest, const char *src, size_t size);
-size_t			ft_strlcat(char *dest, const char *src, size_t size);
-char			*ft_strnstr(const char *str, const char *to_find, size_t len);
+
+/*
+** GNL
+*/
+
+int				ft_get_next_line(int fd, char **line);
+
+/*
+** Int
+*/
+
 int				ft_atoi(const char *str);
-size_t			ft_intlen(long nbr);
-void			*ft_memset(void *s, int c, size_t len);
-void			ft_bzero(void *s, size_t n);
-void			*ft_memcpy(void *dst, const void *src, size_t n);
-void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void			*ft_memmove(void *dest, const void *src, size_t n);
-void			*ft_memchr(const void *s, int c, size_t n);
-int				ft_memcmp(const void *s1, const void *s2, size_t n);
-char			*ft_strrchr(const char *s, int c);
-char			*ft_strchr(const char *s, int c);
-void			*ft_calloc(size_t count, size_t size);
-char			*ft_strdup(const char *c);
-char			*ft_substr(const char *s, unsigned int start, size_t len);
-char			*ft_strjoin(const char *s1, const char *s2);
-char			*ft_strtrim(const char *s1, const char *s2);
-char			**ft_split(const char *s, char c);
 char			*ft_itoa(int n);
-char			*ft_strmapi(const char *s, char (*f)(unsigned int, char));
-void			ft_putchar_fd(char c, int fd);
-void			ft_putstr_fd(char *s, int fd);
-void			ft_putendl_fd(char *s, int fd);
-void			ft_putnbr_fd(int n, int fd);
+size_t			ft_intlen(long nbr);
+size_t			ft_uintlen(unsigned long long nbr, int baselen);
+char			*ft_uitoa(unsigned long long nbr);
+char			*ft_ull_base(unsigned long long nbr, char *base);
+
+/*
+** List
+*/
+
 t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **alst, t_list *new);
 int				ft_lstsize(t_list *list);
@@ -81,13 +78,54 @@ void			ft_lstclear(t_list **lst, void (*del)(void*));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
 t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), \
 				void (*del)(void *));
-char			*ft_uitoa(unsigned long long nbr);
-size_t			ft_uintlen(unsigned long long nbr, int baselen);
-char			*ft_ull_base(unsigned long long nbr, char *base);
+
+/*
+** Memory
+*/
+
+void			*ft_memset(void *s, int c, size_t len);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void			*ft_memmove(void *dest, const void *src, size_t n);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+void			*ft_calloc(size_t count, size_t size);
+
+/*
+** Print
+*/
+
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
+
+/*
+** String
+*/
+
+size_t			ft_strlen(const char *str);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+int				ft_strcmp(const char *s1, const char *s2);
+size_t			ft_strlcpy(char *dest, const char *src, size_t size);
+size_t			ft_strlcat(char *dest, const char *src, size_t size);
+char			*ft_strnstr(const char *str, const char *to_find, size_t len);
+char			*ft_strrchr(const char *s, int c);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strdup(const char *c);
+char			*ft_substr(const char *s, unsigned int start, size_t len);
+char			*ft_strjoin(const char *s1, const char *s2);
+char			*ft_strtrim(const char *s1, const char *s2);
+char			**ft_split(const char *s, char c);
+char			*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 char			*ft_strndup(const char *s, size_t n);
 int				ft_strichr(const char *s, int c);
-int				ft_get_next_line(int fd, char **line);
 void			free_split(char **ret);
+
+/*
+** Text colors
+*/
 
 # define BLACK "\e[0;30m"
 # define RED "\e[0;31m"
